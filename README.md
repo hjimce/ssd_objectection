14d0e3fbdc6094dba3480b213f0245bf5b00379b
大黄蜂 <dahuangfeng@huanleguang.com>
2017-09-12 15:48:09 +0800

    first

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # ssd_objectection
 ssd 训练自己的数据
+
+ 1.标签文件格式转换， 运行函数：
+ ``process.py ->convert_xml2txt('data/my_data/Annotations')``
+ 
+ 把xml标签文件转换成txt文件：比如把2013.xml->2013.txt,txt内容为：
+``` stylus
+物体名字1 minx miny maxx maxy
+物体名字2 minx miny maxx maxy
+```
+ 2. 训练、测试文件名字列表生成train.txt、test.txt以及测试数据的信息文件test_name_size.txt，运行函数：
+ 
+
+``` stylus
+#参数：txt标签文件存放目录，图片存放目录，生成的列表文件输出目录
+process.py->create_list('data/my_data/label_txt','data/my_data/images','data/my_data/')
+```
+
+train.txt 和test.txt,内容格式为：
+
+``` stylus
+data/my_data/images/hlg_00000032964.jpg data/my_data/label_txt/hlg_00000032964.txt
+data/my_data/images/hlg_00000027797.jpg data/my_data/label_txt/hlg_00000027797.txt
+data/my_data/images/hlg_00000028611.jpg data/my_data/label_txt/hlg_00000028611.txt
+data/my_data/images/hlg_00000002065.jpg data/my_data/label_txt/hlg_00000002065.txt
+```
+test_name_size.txt内容格式为：
+
+``` stylus
+图片文件名(不包含扩展名)	   图片宽  图片高
+
+```
+
+
+
+ 3. 根据识别物体任务，修改：labelmap_dataset.prototxt文件
+ 4. 修改 train_ssdi.py文件的相关参数、文件路径等
+
+ 
+
---
+++ preprocess.py
@@ -0,0 +1,68 @@
+#coding=utf-8
+import os
+from  bs4 import  BeautifulSoup
+from PIL import Image
+#把物体检查的bbox训练数据，转换成txt格式
+def convert_xml2txt(xml_root):
+    xmls_files=os.listdir(xml_root)
+    for x in xmls_files:
+        with open(os.path.join(xml_root,x)) as f:
+            y=BeautifulSoup(f.read())
+            image_name=y.annotation.filename.getText()
+            if os.path.exists('label_txt') is False:
+                os.mkdir('label_txt')
+            txt_name=os.path.join('label_txt',os.path.splitext(image_name)[0]+'.txt')
+
+            with open(txt_name,'w') as txt_f:
+                for object in  y.find_all('object'):
+                    for n in object.find_all('name'):
+                        object_name=n.getText()
+                    txt_f.write(object_name+' ')
+                    txt_f.write(object.bndbox.xmin.getText()+' ')
+                    txt_f.write(object.bndbox.ymin.getText() + ' ')
+                    txt_f.write(object.bndbox.xmax.getText() + ' ')
+                    txt_f.write(object.bndbox.ymax.getText() + ' ')
+                    txt_f.write('\n')
+
+
+def create_list(label_txt_root,image_root,out_root,split_ratio=0.9,):
+    label_files=os.listdir(label_txt_root)
+    image_files=os.listdir(image_root)
+
+    label_image_map=[]
+    for l in label_files:
+        label_file=os.path.join(label_txt_root,l)
+        image_file=os.path.join(image_root,os.path.splitext(l)[0]+'.jpg')
+        if os.path.exists(image_file) is False:
+            print 'image no exist'
+            continue
+        label_image_map.append({'label':label_file,'image':image_file})
+    num_test=int(split_ratio*len(label_image_map))
+    train_list=label_image_map[:num_test]
+    test_list=label_image_map[num_test:]
+
+    with open(os.path.join(out_root,'train.txt'),'w') as train_f:
+        for m in train_list:
+            train_f.write(m['image']+' '+m['label']+'\n')
+    with open(os.path.join(out_root, 'test.txt'), 'w') as text_f:
+        for m in test_list:
+            text_f.write(m['image'] + ' ' + m['label'] + '\n')
+    with open(os.path.join(out_root, 'test_name_size.txt'), 'w') as text_f:
+        for m in test_list:
+            size= Image.open(m['image']).size
+            text_f.write(os.path.splitext(m['image'])[0]+' ')
+            text_f.write(str(size[1])+' '+str(size[0])+'\n')
+
+
+
+
+
+
+
+
+
+
+
+#convert_xml2txt('data/my_data/Annotations')
+#参数：txt标签文件存放目录，图片存放目录，生成的列表文件输出目录
+create_list('data/my_data/label_txt','data/my_data/images','data/my_data/')
